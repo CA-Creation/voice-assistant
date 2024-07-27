206ea2f1eb839407a3d8a6e605c75ba11b62857e
Chamodya B Herath <134180331+CA-Creation@users.noreply.github.com>
2024-07-27 12:08:54 +0530

    added voice wakeup

--- engine/command.py
+++ engine/command.py
@@ -38,19 +38,22 @@ def takecommand():
     return query.lower()
 
 @eel.expose
-def allCommands(message=1):
-    query = takecommand()
-    print(query)
-    
-    if "open" in query:
-        from engine.features import openCommand
-        openCommand(query)
-    
-    elif "on youtube":
-        from engine.features import PlayYoutube
-        PlayYoutube(query)
-
-    else:
-        print("not run")
+def allCommands():
+    try:
+        query = takecommand()
+        print(query)
         
+        if "open" in query:
+            from engine.features import openCommand
+            openCommand(query)
+        
+        elif "on youtube":
+            from engine.features import PlayYoutube
+            PlayYoutube(query)
+
+        else:
+            print("not run")
+    except:
+        print("error")
+    
     eel.ShowHood()
